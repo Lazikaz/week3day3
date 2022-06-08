70cc23a382e32c062edb4d18c2400232d62e3f9e
Lazikaz <souflakey@gmail.com>
2022-06-08 15:39:53 -0600

    finished week 3 assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+let message = document.querySelector("#message")
+
+function addMovie(event){
+    event.preventDefault()
+    let inputField = document.querySelector("input").value
+    let movie = document.createElement("li")
+    let movieTitle = document.createElement("span")
+    movieTitle.textContent = inputField
+    movieTitle.addEventListener("click", crossOffMovie)
+    movie.appendChild(movieTitle)
+    let deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener("click", deleteMovie)
+    let ul = document.querySelector("ul")
+    ul.appendChild(movie)
+    movie.appendChild(deleteBtn)
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+    message.textContent = "Movie deleted!"
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle("checked")
+    if(event.target.classList.contains("checked")){
+        message.textContent = "Movie watched!"
+    }else{
+        message.textContent = "Movie added back!"
+    }
+}
+
+let form = document.querySelector("form")
+let button = document.querySelector("button")
+form.addEventListener("submit", addMovie)
